101a028d33277e4bdc3fc67621f120bd086f6b6a
Oscar Brännström Iverbring <oscarbrannstromiverbring@elev.ga.ntig.se>
2025-05-05 14:55:40 +0200

    börjat med Ceasar-krypterare

--- Ceasar/App.xaml.cs
+++ Ceasar/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace Ceasar_krypterare;
+namespace Ceasar;
 
 /// <summary>
 /// Interaction logic for App.xaml
---
+++ Ceasar/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ceasar;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void KlickKryptera(object sender, RoutedEventArgs e)
+    {
+        string inputText = txbInput.Text;
+        string keyText = txbKey.Text;
+
+        int.TryParse(keyText, out int key);
+
+        txbOutput.Clear();
+
+        foreach (var bokstav in inputText)
+        {
+            int ascii = (int)bokstav;
+
+            int asciiKrypterad = ascii + key;
+            char bokstavKrypterad = (char)asciiKrypterad;
+
+            txbOutput.Text += $"{bokstavKrypterad}";
+        }
+    }
+}
